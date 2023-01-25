8b58f2aec62374ebd028b62b7b325d305c99ca97
kt1045 <jp10450317@gmail.com>
2023-01-26 08:42:27 +0900

    Add リレーション

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,13 +9,17 @@ class Post extends Model
 {
     use HasFactory;
     use SoftDeletes;
-    protected $fillable = [
-        'title',
-        'body',
-    ];
-    public function getPaginateByLimit(int $limit_count = 10)
-    {
-        
-        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
-    }
+   protected $fillable = [
+    'title',
+    'body',
+    'category_id'
+];
+    public function getPaginateByLimit(int $limit_count = 5)
+{
+    return $this::with('category')->orderBy('updated_at', 'DESC')->paginate($limit_count);
+}
+    public function category()
+{
+    return $this->belongsTo(Category::class);
+}
 }
